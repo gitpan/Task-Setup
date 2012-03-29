ed90b06a77fc37c99e3692d4f93cd057fbc13c5e
Steven Haryanto <stevenharyanto@gmail.com>
2012-03-29 01:58:39 +0000

    Import of SHARYANTO/Task-Setup-0.01 from CPAN.
    gitpan-cpan-distribution: Task-Setup
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Task-Setup-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Setup.pm
@@ -0,0 +1,56 @@
+package Task::Perinci;
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: Install all Setup modules
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::Perinci - Install all Setup modules
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+This task will install all Setup modules.
+
+=head1 TASK CONTENTS
+
+=head2 Included modules
+
+=head3 L<Setup>
+
+=head3 L<Setup::File>
+
+=head3 L<Setup::File::Symlink>
+
+=head3 L<Setup::Multi>
+
+=head3 L<Setup::Test>
+
+=head3 L<Setup::Text::Snippet>
+
+=head3 L<Setup::Unix::User>
+
+=head3 L<Test::Setup>
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
